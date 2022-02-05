080f5eb78fef2b18577982020a09d622c3fa0f4b
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-02-05 11:02:23 +0100

    Søknadsperiode funk

--- src/app/utils/dates.ts
+++ src/app/utils/dates.ts
@@ -1,4 +1,5 @@
 import { DateRange } from '@navikt/sif-common-formik/lib';
+import { SoknadFormData } from '../types/SoknadFormData';
 import dayjs from 'dayjs';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 
@@ -19,4 +20,52 @@ export const getDatesWithinDateRange = ({ from, to }: DateRange): Date[] => {
         currentDate = dayjs(currentDate).add(1, 'day').toDate();
     }
     return dates;
+};
+
+export const getSøknadsperiodeFromFormData = ({
+    fraværPerioder,
+    fraværDager,
+}: Partial<SoknadFormData>): DateRange | undefined => {
+    if ((fraværPerioder && fraværPerioder.length > 0) || (fraværDager && fraværDager.length > 0)) {
+        const minstFraDatoFraværPerioder =
+            fraværPerioder && fraværPerioder?.length > 0
+                ? fraværPerioder.reduce((a, b) => (a.fraOgMed < b.fraOgMed ? a : b)).fraOgMed
+                : undefined;
+        const minstFraværDag =
+            fraværDager && fraværDager.length > 0
+                ? fraværDager.reduce((a, b) => (a.dato < b.dato ? a : b)).dato
+                : undefined;
+        const maxTilDatoFraværPerioder =
+            fraværPerioder && fraværPerioder.length > 0
+                ? fraværPerioder.reduce((a, b) => (a.tilOgMed > b.tilOgMed ? a : b)).tilOgMed
+                : undefined;
+        const maxFraværDag =
+            fraværDager && fraværDager.length > 0
+                ? fraværDager.reduce((a, b) => (a.dato > b.dato ? a : b)).dato
+                : undefined;
+
+        const søknadsperiode = (): DateRange | undefined => {
+            if (minstFraDatoFraværPerioder && minstFraværDag && maxTilDatoFraværPerioder && maxFraværDag) {
+                return {
+                    from: dayjs(minstFraDatoFraværPerioder).isBefore(minstFraværDag)
+                        ? minstFraDatoFraværPerioder
+                        : minstFraværDag,
+                    to: dayjs(maxTilDatoFraværPerioder).isAfter(maxFraværDag) ? maxTilDatoFraværPerioder : maxFraværDag,
+                };
+            } else if (minstFraDatoFraværPerioder && maxTilDatoFraværPerioder && !minstFraværDag && !maxFraværDag) {
+                return {
+                    from: minstFraDatoFraværPerioder,
+                    to: maxTilDatoFraværPerioder,
+                };
+            } else if (!minstFraDatoFraværPerioder && !maxTilDatoFraværPerioder && minstFraværDag && maxFraværDag) {
+                return {
+                    from: minstFraværDag,
+                    to: maxFraværDag,
+                };
+            } else return undefined;
+        };
+        return søknadsperiode();
+    }
+
+    return undefined;
 };
