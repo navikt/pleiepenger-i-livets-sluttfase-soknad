592bf7a854bb6dfa210e811c3c893827fdb92a7f
Aleksei Poroshin <aleksei.poroshin@nav.no>
2022-02-10 15:51:38 +0100

    validering 60 dager

--- src/app/soknad/fravær-step/fraværFieldValidations.ts
+++ src/app/soknad/fravær-step/fraværFieldValidations.ts
@@ -3,6 +3,7 @@ import { ValidationError } from '@navikt/sif-common-formik/lib/validation/types'
 import { validateAll } from '@navikt/sif-common-formik/lib/validation/validationUtils';
 import { FraværPeriode } from '@navikt/sif-common-forms/lib';
 import { validateNoCollisions } from '@navikt/sif-common-forms/lib/fravær/fraværValidationUtils';
+import dayjs from 'dayjs';
 
 enum FraværErrors {
     ulikeÅrstall = 'ulikeÅrstall',
@@ -11,8 +12,18 @@ enum FraværErrors {
 }
 
 export const getFraværPerioderValidator = () => (fraværPerioder: FraværPeriode[]) => {
+    console.log('fraværPerioder: ', fraværPerioder);
     return validateAll<ValidationError>([
         () => getListValidator({ required: true })(fraværPerioder),
         () => (validateNoCollisions([], fraværPerioder) ? FraværErrors.perioderEllerDagerOverlapper : undefined),
+        // Vet ikke om 60 arbeidsdager eller ikke
+        // () => (perioderMerEnn60dager(fraværPerioder) ? FraværErrors.merEnn60dager : undefined),
     ]);
+};
+
+export const perioderMerEnn60dager = (fraværPerioder: FraværPeriode[]) => {
+    const sum = fraværPerioder.reduce(function (accumulator, currentValue) {
+        return accumulator + dayjs(currentValue.tilOgMed).diff(currentValue.fraOgMed, 'days');
+    }, 0);
+    return sum > 60;
 };
